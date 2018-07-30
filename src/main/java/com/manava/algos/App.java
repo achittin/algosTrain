9a0a2c24f1db9b90c2c329dfb2450aff642c57bb
ac027466 <avaneesh.chittineni@cerner.com>
2018-07-30 10:37:00 -0500

    Adding logger to the hello world

--- src/main/java/com/manava/algos/App.java
+++ src/main/java/com/manava/algos/App.java
@@ -1,11 +1,15 @@
 package com.manava.algos;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world.
  *
  */
 public class App {
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
   public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
+    LOGGER.info( "Hello World!" );
   }
 }
