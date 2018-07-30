dbe5fd73f673267cf4c4c4649d01b847afffc9d6
Avaneesh Chittineni <avaneesh.ch@gmail.com>
2018-07-30 13:15:51 -0500

    Initial dev (#1)
    * Updating gitignore
    
    * Initiating maven project
    
    * Adding logger to the hello world
    
    * Uplifting junit

---
+++ src/main/java/com/manava/algos/App.java
@@ -0,0 +1,15 @@
+package com.manava.algos;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world.
+ *
+ */
+public class App {
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+  public static void main( String[] args ) {
+    LOGGER.info( "Hello World!" );
+  }
+}
